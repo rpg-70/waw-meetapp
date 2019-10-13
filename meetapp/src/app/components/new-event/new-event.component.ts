ddba2681b052282bbd274f2025f184a27c94fd5d
piotr <piotr.guziorski@gmail.com>
2019-10-13 16:33:42 +0200

    Fix for adding stuff

--- meetapp/src/app/components/new-event/new-event.component.ts
+++ meetapp/src/app/components/new-event/new-event.component.ts
@@ -17,7 +17,6 @@ export class NewEventComponent implements OnInit {
   showFriendsInPlace: boolean = false;
   places
   chosenCategory: string;
-  myEvents = [];
   constructor(
     private _formBuilder: FormBuilder,
     private http: HttpClient,
@@ -46,11 +45,7 @@ export class NewEventComponent implements OnInit {
     });
   }
   saveEvent() {
-    if (localStorage.getItem('new-event-main-info')) {
-      this.myEvents = JSON.parse(localStorage.getItem('new-event-main-info'));
-    }
-
-    this.myEvents.push(JSON.stringify(this.mainInformation.value));
+    localStorage.setItem('new-event-main-info', JSON.stringify(this.mainInformation.value));
     localStorage.setItem('my-events', JSON.stringify(this.myEvents));
     localStorage.setItem('new-event-participants', JSON.stringify(this.participantsInformation.value));
     localStorage.setItem('new-event-location', JSON.stringify(this.locationInformation.value));
